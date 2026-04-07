44b3a0638524569ed0c28019b4092dcaf82c9006
Sumanta Rakshit <144620929+sumantahitk@users.noreply.github.com>
2026-04-07 18:11:02 +0530

    Time: 1 ms (98.15%) | Memory: 42.6 MB (80.56%) - LeetSync

---
+++ 1070-convert-to-base-2/convert-to-base-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String baseNeg2(int n) {
+         if (n == 0) return "0";
+        StringBuilder sb=new StringBuilder();
+        while(n!=0){
+               int rem = n % -2;
+            n /= -2;
+
+            if (rem < 0) {
+                rem += 2;
+                n += 1;
+            }
+
+            sb.append(rem);
+        
+        }
+       sb.reverse();
+        return sb.toString();
+    }
+}

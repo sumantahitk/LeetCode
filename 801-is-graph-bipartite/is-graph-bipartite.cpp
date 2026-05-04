d33264afa236c985ab37dde7a4420fe3ea542425
Sumanta Rakshit <144620929+sumantahitk@users.noreply.github.com>
2026-05-04 08:08:27 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (79.02%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& g) {
+    bool m1(vector<vector<int>>& g) {
         int n=g.size();
         queue<int>q;
         
@@ -25,5 +25,33 @@ public:
             }
         }
         return 1;
+    }
+    bool dfs(int node,vector<int>&col,vector<vector<int>>&g){
+        
+        for(int i=0;i<g[node].size();i++){
+            if(col[g[node][i]]==-1){
+                col[g[node][i]]=!col[node];
+                 if (!dfs(g[node][i], col, g)) return false;
+            }
+           
+            else if(col[node]==col[g[node][i]]) return 0;
+        }
+        return 1;
+    }
+    bool m2(vector<vector<int>>& g) {
+        int n=g.size();
+        vector<int>col(n,-1);
+        for(int i=0;i<n;i++){
+            if(col[i]==-1){
+                col[i]=1;
+                if(!dfs(i,col,g))return 0;
+            }
+        }
+        return 1;
+    }
+     bool isBipartite(vector<vector<int>>& g) {
+       
+       return m2(g);
+         
     }
 };

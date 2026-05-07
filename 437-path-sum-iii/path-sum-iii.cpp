79b1a7e2881275db968bec12afa72f532aab0412
Sumanta Rakshit <144620929+sumantahitk@users.noreply.github.com>
2026-05-07 12:12:40 +0530

    Time: 11 ms (36.18%) | Memory: 21.8 MB (13.63%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -1,32 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int cnt=0;
-    int find(TreeNode* root,int tar,long long sum,int &cnt){
-        if(!root) return cnt;
-        sum+=root->val;
-        if(tar==sum){
-            cnt++;
-        }
-        return find(root->left,tar,sum,cnt)+ find(root->right,tar,sum,cnt);
+    int dfs(TreeNode* root, long long currSum, int target,
+            unordered_map<long long, int>& mp) {
+        if (!root) return 0;
+
+        currSum += root->val;
+
+        int count = mp[currSum - target];
+
+        mp[currSum]++;
+
+        count += dfs(root->left, currSum, target, mp);
+        count += dfs(root->right, currSum, target, mp);
+
+        mp[currSum]--; // backtrack
+
+        return count;
     }
-    int pathSum(TreeNode* root, int tar) {
-        if(!root) return 0;
-        long long sum=0;
-        int cnt=0;
-        find(root,tar,sum,cnt);
-        if(root->left)cnt+= pathSum(root->left,tar);
-        if(root->right) cnt+=pathSum(root->right,tar);
-        return cnt;
+
+    int pathSum(TreeNode* root, int targetSum) {
+        unordered_map<long long, int> mp;
+        mp[0] = 1;
+        return dfs(root, 0, targetSum, mp);
     }
 };

6787a402e749746482a269a8b0454cab1a9c0329
Sumanta Rakshit <144620929+sumantahitk@users.noreply.github.com>
2026-05-11 07:14:11 +0530

    Time: 133 ms (6.98%) | Memory: 109.5 MB (5.38%) - LeetSync

---
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+      int find(int i,int buy,int n,vector<int>&p,vector<vector<int>>&v,int &fee){
+        if(i==n) return 0;
+        if(v[i][buy]!=-1) return v[i][buy];
+
+        if(buy){
+            v[i][buy]=max(-p[i]+find(i+1,0,n,p,v,fee),find(i+1,1,n,p,v,fee));
+        }else{
+            v[i][buy]=max(p[i]+find(i+1,1,n,p,v,fee)-fee,0+find(i+1,0,n,p,v,fee));
+        }
+        return v[i][buy];
+    }
+    int maxProfit(vector<int>& p, int fee) {
+         int i=0,n=p.size();
+        vector<vector<int>>v(n+1,vector<int>(2,-1));
+        return find(0,1,n,p,v,fee);
+    }
+};
